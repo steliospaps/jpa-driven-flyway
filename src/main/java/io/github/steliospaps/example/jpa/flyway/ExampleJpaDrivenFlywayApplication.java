819a74710398a029a8b49780ceea48ce56f1f86a
stelios <stelios05@gmail.com>
2021-07-10 18:22:53 +0100

    spring initializer

---
+++ src/main/java/io/github/steliospaps/example/jpa/flyway/ExampleJpaDrivenFlywayApplication.java
@@ -0,0 +1,13 @@
+package io.github.steliospaps.example.jpa.flyway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleJpaDrivenFlywayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleJpaDrivenFlywayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/steliospaps/example/jpa/flyway/ExampleJpaDrivenFlywayApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.steliospaps.example.jpa.flyway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleJpaDrivenFlywayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
